2766b76797e8ac17963705931f3c7ca3245065da
Moti Bartov <motib@tikalk.com>
2019-12-15 21:12:50 +0200

    Commit for all changes

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/motibartov/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/motibartov/Work/Private/tikal_time_tracker"
+export "FLUTTER_TARGET=/Users/motibartov/Work/Private/tikal_time_tracker/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/motibartov/Development/flutter/bin/cache/artifacts/engine/ios"
