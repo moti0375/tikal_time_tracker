4a9d512eb71481d2414560776da0f517ac026b88
Moti Bartov <motib@tikalk.com>
2020-01-28 16:24:09 +0200

    Initialize commit

---
+++ lib/network/time_tracker_api.jretro.dart
@@ -0,0 +1,84 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'time_tracker_api.dart';
+
+// **************************************************************************
+// JaguarHttpGenerator
+// **************************************************************************
+
+/* NoSuchMethodError: The getter 'type' was called on null.
+Receiver: null
+Tried calling: type 
+
+ #0      Object.noSuchMethod (dart:core-patch/object_patch.dart:53:5)
+#1      parse (package:jaguar_retrofit_gen/parser/parser.dart:141:45)
+#2      JaguarHttpGenerator.generateForAnnotatedElement (package:jaguar_retrofit_gen/generator/generator.dart:26:22)
+#3      GeneratorForAnnotation.generate (package:source_gen/src/generator_for_annotation.dart:47:30)
+#4      _generate (package:source_gen/src/builder.dart:280:35)
+<asynchronous suspension>
+#5      _Builder._generateForLibrary (package:source_gen/src/builder.dart:73:15)
+#6      _Builder.build (package:source_gen/src/builder.dart:67:11)
+<asynchronous suspension>
+#7      runBuilder.buildForInput (package:build/src/generate/run_builder.dart:55:21)
+#8      MappedListIterable.elementAt (dart:_internal/iterable.dart:417:29)
+#9      ListIterator.moveNext (dart:_internal/iterable.dart:346:26)
+#10     Future.wait (dart:async/future.dart:393:26)
+#11     runBuilder.<anonymous closure> (package:build/src/generate/run_builder.dart:61:36)
+#12     _rootRun (dart:async/zone.dart:1126:13)
+#13     _CustomZone.run (dart:async/zone.dart:1023:19)
+#14     _runZoned (dart:async/zone.dart:1518:10)
+#15     runZoned (dart:async/zone.dart:1502:12)
+#16     scopeLogAsync (package:build/src/builder/logging.dart:22:3)
+#17     runBuilder (package:build/src/generate/run_builder.dart:61:9)
+#18     _SingleBuild._runForInput.<anonymous closure>.<anonymous closure>.<anonymous closure> (package:build_runner_core/src/generate/build_impl.dart:487:19)
+#19     _NoOpBuilderActionTracker.trackStage (package:build_runner_core/src/generate/performance_tracker.dart:300:15)
+#20     _SingleBuild._runForInput.<anonymous closure>.<anonymous closure> (package:build_runner_core/src/generate/build_impl.dart:485:23)
+<asynchronous suspension>
+#21     NoOpTimeTracker.track (package:timing/src/timing.dart:222:44)
+#22     _SingleBuild._runForInput.<anonymous closure> (package:build_runner_core/src/generate/build_impl.dart:442:22)
+#23     Pool.withResource (package:pool/pool.dart:127:28)
+<asynchronous suspension>
+#24     _SingleBuild._runForInput (package:build_runner_core/src/generate/build_impl.dart:438:17)
+#25     _SingleBuild._runBuilder.<anonymous closure> (package:build_runner_core/src/generate/build_impl.dart:376:38)
+#26     MappedIterator.moveNext (dart:_internal/iterable.dart:395:18)
+#27     Future.wait (dart:async/future.dart:393:26)
+#28     _SingleBuild._runBuilder (package:build_runner_core/src/generate/build_impl.dart:375:36)
+#29     _AsyncAwaitCompleter.start (dart:async-patch/async_patch.dart:45:6)
+#30     _SingleBuild._runBuilder (package:build_runner_core/src/generate/build_impl.dart:373:40)
+#31     _SingleBuild._runPhases.<anonymous closure>.<anonymous closure> (package:build_runner_core/src/generate/build_impl.dart:321:20)
+#32     _asyncThenWrapperHelper.<anonymous closure> (dart:async-patch/async_patch.dart:73:64)
+#33     _rootRunUnary (dart:async/zone.dart:1134:38)
+#34     _CustomZone.runUnary (dart:async/zone.dart:1031:19)
+#35     _FutureListener.handleValue (dart:async/future_impl.dart:139:18)
+#36     Future._propagateToListeners.handleValueCallback (dart:async/future_impl.dart:680:45)
+#37     Future._propagateToListeners (dart:async/future_impl.dart:709:32)
+#38     Future._completeWithValue (dart:async/future_impl.dart:524:5)
+#39     _AsyncAwaitCompleter.complete (dart:async-patch/async_patch.dart:32:15)
+#40     _completeOnAsyncReturn (dart:async-patch/async_patch.dart:290:13)
+#41     _SingleBuild._matchingPrimaryInputs (package:build_runner_core/src/generate/build_impl.dart)
+#42     _asyncThenWrapperHelper.<anonymous closure> (dart:async-patch/async_patch.dart:73:64)
+#43     _rootRunUnary (dart:async/zone.dart:1134:38)
+#44     _CustomZone.runUnary (dart:async/zone.dart:1031:19)
+#45     _FutureListener.handleValue (dart:async/future_impl.dart:139:18)
+#46     Future._propagateToListeners.handleValueCallback (dart:async/future_impl.dart:680:45)
+#47     Future._propagateToListeners (dart:async/future_impl.dart:709:32)
+#48     Future._completeWithValue (dart:async/future_impl.dart:524:5)
+#49     Future.wait.<anonymous closure> (dart:async/future.dart:400:22)
+#50     _rootRunUnary (dart:async/zone.dart:1134:38)
+#51     _CustomZone.runUnary (dart:async/zone.dart:1031:19)
+#52     _FutureListener.handleValue (dart:async/future_impl.dart:139:18)
+#53     Future._propagateToListeners.handleValueCallback (dart:async/future_impl.dart:680:45)
+#54     Future._propagateToListeners (dart:async/future_impl.dart:709:32)
+#55     Future._completeWithValue (dart:async/future_impl.dart:524:5)
+#56     Future._asyncComplete.<anonymous closure> (dart:async/future_impl.dart:554:7)
+#57     _rootRun (dart:async/zone.dart:1126:13)
+#58     _CustomZone.run (dart:async/zone.dart:1023:19)
+#59     _CustomZone.runGuarded (dart:async/zone.dart:925:7)
+#60     _CustomZone.bindCallbackGuarded.<anonymous closure> (dart:async/zone.dart:965:23)
+#61     _microtaskLoop (dart:async/schedule_microtask.dart:43:21)
+#62     _startMicrotaskLoop (dart:async/schedule_microtask.dart:52:5)
+#63     _runPendingImmediateCallback (dart:isolate-patch/isolate_patch.dart:118:13)
+#64     _RawReceivePortImpl._handleMessage (dart:isolate-patch/isolate_patch.dart:175:5)
+ 
+
+ */
